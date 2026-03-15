3e001708788677afaf0b086821507784f52e65f3
jorgearma <jorgesiemprearmando@gmail.com>
2026-03-15 03:44:31 +0100

    refactorizando DB

--- database.py
+++ database.py
@@ -1,57 +1,11 @@
 import os
-import pyodbc
 import urllib.parse
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class DatabaseConnection:
-    def __init__(self, driver, server, database, uid, pwd):
-        self.driver = driver
-        self.server = server
-        self.database = database
-        self.uid = uid
-        self.pwd = pwd
-        self.connection = None
-
-    def connect(self):
-        try:
-            self.connection = pyodbc.connect(
-                f"DRIVER={{{self.driver}}};"
-                f"SERVER={self.server};"
-                f"DATABASE={self.database};"
-                f"UID={self.uid};"
-                f"PWD={self.pwd}"
-            )
-            print("Conexión exitosa a la base de datos SQL Server")
-            return self.connection
-        except Exception as e:
-            print("Error al conectar con la base de datos:", e)
-            return None
-
-    def close(self):
-        if self.connection:
-            self.connection.close()
-            print("Conexión cerrada")
-
-# Función global para mantener compatibilidad
-def conectar_bd():
-    db = DatabaseConnection(
-        driver="ODBC Driver 18 for SQL Server",
-        server=os.environ.get('SQL_SERVER', 'localhost,1433'),
-        database=os.environ.get('SQL_DATABASE', 'pruebabot'),
-        uid=os.environ.get('SQL_UID', 'sa'),
-        pwd=os.environ.get('SQL_PWD', '')
-    )
-    return db.connect()
-
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-# Definir la base para los modelos
-Base = declarative_base()
 
 params = urllib.parse.quote_plus(
     f"DRIVER=ODBC Driver 18 for SQL Server;"
