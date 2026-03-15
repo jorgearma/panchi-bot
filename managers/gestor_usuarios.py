c0b6f12b579ef22e53ba10d4e9767873d4ef9044
jorgearma <jorgesiemprearmando@gmail.com>
2026-03-15 18:11:47 +0100

    gestor de usuarios mejorado

--- managers/gestor_usuarios.py
+++ managers/gestor_usuarios.py
@@ -1,10 +1,8 @@
-from utils.menu_opciones import mostrar_menu
-from utils.mensajes import enviar_mensaje_whatsapp
 from sqlalchemy.exc import SQLAlchemyError, OperationalError
 from models import Usuario
 from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
 
-class GestorUsuariosBD:
+class GestorUsuarios:
 
     @property
     def session(self):
@@ -67,56 +65,3 @@ class GestorUsuariosBD:
             print("Error al verificar el usuario:", e)
             raise
         
-
-
-class GestorUsuarios:
-
-    @staticmethod
-    def manejar_usuario(numero_cliente , usuario_datos):
-        """
-        Maneja la interacción con un usuario basado en su número de cliente.
-        si no hay un pedido iniciado, se le envía el menú y se inicia un nuevo pedido.
-
-        Este método realiza las siguientes acciones:
-        1. Intenta obtener los datos del usuario desde la base de datos, con un máximo de reintentos.
-        2. Verifica si el usuario está recién registrado y maneja ese caso.
-        3. Si el usuario no tiene un pedido pendiente, inicia un nuevo pedido y envía el menú.
-
-        Args:
-            numero_cliente (str): El número de cliente del usuario.
-
-        Returns:
-            tuple: Una tupla que contiene un mensaje de estado (str) y un código HTTP (int).
-                   - En caso de error en la base de datos, devuelve un mensaje de error y el código 500.
-                   - Si el usuario está recién registrado, devuelve un mensaje de confirmación y el código 200.
-                   - Si se envía el menú correctamente, devuelve un mensaje de éxito y el código 200.
-
-        Raises:
-            SQLAlchemyError: Si ocurre un error al interactuar con la base de datos.
-        """
-        from services import gestor_pedidos
-
-
-        id_usuario = usuario_datos["id"]
-        direccion_usuario = usuario_datos["direccion"]
-        nombre_usuario = usuario_datos["nombre"]
-        
-
-        # Si no tiene un pedido pendiente, lo iniciamos y enviamos el menú
-        
-        try:
-            gestor_pedidos.iniciar_pedido(id_usuario, direccion_usuario, nombre_usuario)
-            
-        except (SQLAlchemyError, OperationalError) as error:
-            # Captura los errores después de que el decorador haya agotado los intentos
-            print(f"Error al iniciar el pedido para el usuario {id_usuario}: {error}")
-            return "Error al procesar el pedido. Inténtalo más tarde.", 500
-        
-        menu_texto = mostrar_menu()
-
-        enviar_mensaje_whatsapp(
-                f"¡Hola *{nombre_usuario}*! 🙂 {menu_texto} \nescribe el *numero* para elegir ",
-                numero_cliente
-            )
-
-        return "Mensaje enviado", 200
--- services.py
+++ services.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from managers.gestor_pedidos import GestorPedidos
-from managers.gestor_usuarios import GestorUsuariosBD
+from managers.gestor_usuarios import GestorUsuarios
 from managers.gestor_productos import ProductoManager
 from managers.gestor_redis import redismanager
 
 gestor_pedidos = GestorPedidos()
-gestor_usuarios = GestorUsuariosBD()
+gestor_usuarios = GestorUsuarios()
 gestor_productos = ProductoManager()
 
 monei = Monei.MoneiClient(api_key=os.environ.get('MONEI_API_KEY'))
