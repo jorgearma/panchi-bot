2049939291540f8d60863cddaee8414ff864c47f
jorgearma <jorgesiemprearmando@gmail.com>
2026-04-04 06:11:52 +0200

    ui  cliente donde puede ver el estado de su pedido

--- controllers/pago_notifier.py
+++ controllers/pago_notifier.py
@@ -11,7 +11,7 @@ def _enviar_confirmacion_efectivo(numero_cliente, nombre_cliente, total_euros,
         f"▪️Nombre: *{nombre_cliente}*\n"
         f"▪️Importe: *{total_euros}*€\n"
         f"▪️ID pedido: *#{pedido_id}*\n"
-        f"▪️Forma de pago: *Efectivo (a la entrega)*\n"
+        f"▪️Pago: *Efectivo (a la entrega)*\n"
         f"▪️Tiempo estimado: *15m*\n"
         f"▪️Dirección: 👇🏼 \n\n{direccion_cliente}"
     )
