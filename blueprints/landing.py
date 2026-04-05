c07e8e3d9d121161c1555ffda7d353f9f40a568c
jorgearma <jorgesiemprearmando@gmail.com>
2026-04-05 06:48:36 +0200

    landing page terminada

--- blueprints/landing.py
+++ blueprints/landing.py
@@ -13,6 +13,16 @@ def index():
     return send_from_directory(LANDING_DIR, 'index.html')
 
 
+@blueprint_landing.route('/about')
+def about():
+    return send_from_directory(LANDING_DIR, 'about.html')
+
+
+@blueprint_landing.route('/por-que-funciona')
+def por_que_funciona():
+    return send_from_directory(LANDING_DIR, 'por-que-funciona.html')
+
+
 @blueprint_landing.route('/landing/<path:filename>')
 def static_files(filename):
     return send_from_directory(LANDING_DIR, filename)
